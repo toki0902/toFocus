c9d77a57813005fd5e2b8b512fd8be4b678b892f
toki with desktop <toki.shigoto@gmail.com>
2024-05-28 19:14:06 +0900

    🔧 viteconfigにエイリアスを追加しました

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@images": path.resolve("src", "assets", "images"),
+      "@component": path.resolve("src", "components", "index"),
+    },
+  },
+});
