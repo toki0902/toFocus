58dd56223be9d5d4fa40bee6338fa2211ae83f38
toki with desktop <toki.shigoto@gmail.com>
2024-05-29 23:46:25 +0900

    🎨 ルーティングを設定しました。

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -4,11 +4,13 @@ import {
   Routes,
 } from "react-router-dom/dist/react-router-dom.development";
 import Explain from "./features/explain/Explain";
+import Work from "./features/work/Work";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Explain />}></Route>
+      <Route path="/work" element={<Work />}></Route>
     </Routes>
   );
 };
