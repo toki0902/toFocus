5adbdd16d9b7fb125f2bc82ac267f3cf52a84173
toki with desktop <toki.shigoto@gmail.com>
2024-06-04 21:01:09 +0900

    ⚡️ FlexBoxにrefを渡せるようにしました。startDoをtasks更新時に更新するようにしました

--- frontend/src/components/flexbox/FlexBox.jsx
+++ frontend/src/components/flexbox/FlexBox.jsx
@@ -1,101 +1,111 @@
-import React from "react";
+import React, { forwardRef } from "react";
 
 //Flexなwrapを提供する。
 //columnはflex-direction
 //left,right,top.bottomはtrueにすると指定した方向に要素が寄る
 //sbはspace-between
-const FlexBox = ({
-  className = null,
-  children,
-  column = false,
-  left = false,
-  right = false,
-  top = false,
-  bottom = false,
-  ml = 0,
-  mt = 0,
-  mr = 0,
-  mb = 0,
-  sb = false,
-  element = false,
-  width = "auto",
-  height = "auto",
-  onClick = null,
-  onKeyDown = null,
-  onMouseOver = null,
-  onMouseOut = null,
-}) => {
-  const style = column
-    ? {
-        display: "flex",
-        justifyContent: top
-          ? "left"
-          : bottom
-          ? "right"
-          : sb
-          ? "space-between"
-          : "center",
-        alignItems: right ? "end" : left ? "start" : "center",
-        flexDirection: column ? "column" : "row",
-        width: width,
-        height: height,
-        margin: `${mt} ${mr} ${mb} ${ml}`,
-      }
-    : {
-        display: "flex",
-        justifyContent: left
-          ? "left"
-          : right
-          ? "right"
-          : sb
-          ? "space-between"
-          : "center",
-        alignItems: top ? "start" : bottom ? "end" : "center",
-        flexDirection: column ? "column" : "row",
-        width: width,
-        height: height,
-        margin: `${mt} ${mr} ${mb} ${ml}`,
-      };
-  if (element == "ul") {
-    return (
-      <ul
-        style={style}
-        className={className}
-        onClick={onClick}
-        onMouseOver={onMouseOver}
-        onMouseOut={onMouseOut}
-        onKeyDown={onKeyDown}
-      >
-        {children}
-      </ul>
-    );
-  } else if (element == "li") {
-    return (
-      <li
-        style={style}
-        className={className}
-        onClick={onClick}
-        onMouseOver={onMouseOver}
-        onMouseOut={onMouseOut}
-        onKeyDown={onKeyDown}
-      >
-        {children}
-      </li>
-    );
-  } else {
-    return (
-      <div
-        style={style}
-        className={className}
-        onClick={onClick}
-        onMouseOver={onMouseOver}
-        onMouseOut={onMouseOut}
-        onKeyDown={onKeyDown}
-      >
-        {children}
-      </div>
-    );
+//refも継承するためにforwardRefを採用
+const FlexBox = forwardRef(
+  (
+    {
+      className = null,
+      children,
+      column = false,
+      left = false,
+      right = false,
+      top = false,
+      bottom = false,
+      ml = 0,
+      mt = 0,
+      mr = 0,
+      mb = 0,
+      sb = false,
+      element = false,
+      width = "auto",
+      height = "auto",
+      onClick = null,
+      onKeyDown = null,
+      onMouseOver = null,
+      onMouseOut = null,
+    },
+    ref
+  ) => {
+    const style = column
+      ? {
+          display: "flex",
+          justifyContent: top
+            ? "flex-start"
+            : bottom
+            ? "flex-end"
+            : sb
+            ? "space-between"
+            : "center",
+          alignItems: right ? "flex-end" : left ? "flex-start" : "center",
+          flexDirection: "column",
+          width: width,
+          height: height,
+          margin: `${mt} ${mr} ${mb} ${ml}`,
+        }
+      : {
+          display: "flex",
+          justifyContent: left
+            ? "flex-start"
+            : right
+            ? "flex-end"
+            : sb
+            ? "space-between"
+            : "center",
+          alignItems: top ? "flex-start" : bottom ? "flex-end" : "center",
+          flexDirection: "row",
+          width: width,
+          height: height,
+          margin: `${mt} ${mr} ${mb} ${ml}`,
+        };
+
+    if (element === "ul") {
+      return (
+        <ul
+          style={style}
+          className={className}
+          onClick={onClick}
+          onMouseOver={onMouseOver}
+          onMouseOut={onMouseOut}
+          onKeyDown={onKeyDown}
+          ref={ref}
+        >
+          {children}
+        </ul>
+      );
+    } else if (element === "li") {
+      return (
+        <li
+          style={style}
+          className={className}
+          onClick={onClick}
+          onMouseOver={onMouseOver}
+          onMouseOut={onMouseOut}
+          onKeyDown={onKeyDown}
+          ref={ref}
+        >
+          {children}
+        </li>
+      );
+    } else {
+      return (
+        <div
+          style={style}
+          className={className}
+          onClick={onClick}
+          onMouseOver={onMouseOver}
+          onMouseOut={onMouseOut}
+          onKeyDown={onKeyDown}
+          ref={ref}
+        >
+          {children}
+        </div>
+      );
+    }
   }
-};
+);
 
 export default FlexBox;
--- frontend/src/features/work/Work.jsx
+++ frontend/src/features/work/Work.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import SetGoals from "./components/setGoals/SetGoals";
 import Do from "./components/do/Do";
 import Break from "./components/break/Break";
@@ -13,36 +13,39 @@ const Work = () => {
     setWorkingHours_withoutLongBreak_min,
   ] = useState(0);
 
-  console.log(workingHours_min);
-  //目標タスクを追加で一つ設定するための関数
-  //SetGoalsコンポーネントを新規で呼び出す。
-  const continueSetGoals = (task_toAdd, time_toAdd) => {
-    setTasks((prev) => [...prev, task_toAdd]);
-    setRequireTime_min((prev) => prev + time_toAdd);
-    setCurrentStage(
-      <SetGoals
-        Continue
-        continueSetGoals={continueSetGoals}
-        key={Date.now()}
-        startDo={startDo}
-      />
-    );
-  };
-
-  //Doコンポーネントに渡すためのupdate用の関数
-  const updateWorkingTime_useMin = (time_toAdd) => {
-    setWorkingHours_min((prev) => prev + time_toAdd);
-    setWorkingHours_withoutLongBreak_min((prev) => prev + time_toAdd);
-  };
-
-  const startDo = () => {
+  const startDo = useCallback(() => {
     setCurrentStage(
       <Do
         key={Date.now()}
         startBreak={startBreak}
         updateWorkingTime_useMin={updateWorkingTime_useMin}
+        tasks={tasks}
       />
     );
+  }, [tasks]);
+
+  //目標タスクを追加で一つ設定するための関数
+  //SetGoalsコンポーネントを新規で呼び出す。
+  const continueSetGoals = useCallback(
+    (task_toAdd, time_toAdd) => {
+      setTasks((prev) => [...prev, task_toAdd]);
+      setRequireTime_min((prev) => prev + time_toAdd);
+      setCurrentStage(
+        <SetGoals
+          Continue
+          continueSetGoals={continueSetGoals}
+          key={Date.now()}
+          startDo={startDo}
+        />
+      );
+    },
+    [startDo]
+  );
+
+  //Doコンポーネントに渡すためのupdate用の関数
+  const updateWorkingTime_useMin = (time_toAdd) => {
+    setWorkingHours_min((prev) => prev + time_toAdd);
+    setWorkingHours_withoutLongBreak_min((prev) => prev + time_toAdd);
   };
 
   const startBreak = () => {
